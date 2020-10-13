e498af55f2d80cc2c5def48b534631183f4eac95
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-13 15:56:32 +0000

    Final digit finder

--- Main.java
+++ Main.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Tells user their last digit
+ * @author Tolga Nurtekin
  */
 public class Main {
+  //create method for the final digit of users number
+  public static int lastDigit(int userNum){
+    int remainder = userNum % 10;
+    return remainder;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner
+    Scanner input = new Scanner(System.in);
+    //ask user for number
+    System.out.println("Enter Number");
+    //declare a variable to store users number
+    int userNum = input.nextInt();
+    //calculate the final number
+    int remainder = userNum % 10;
+    System.out.println(remainder);
   }
 }
+
